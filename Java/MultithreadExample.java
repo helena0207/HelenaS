a48d7c91915ba973d724254c5259a33b626a1b0d
unknown <dineout@naver.com>
2020-06-02 23:49:55 +0900

    200602-07

---
+++ Java/MultithreadExample.java
@@ -0,0 +1,31 @@
+
+public class MultithreadExample {
+	public static void main(String args[]) {
+		Thread thread1 = new DigitThread();  //스레드 생성
+		Thread thread2 = new DigitThread();
+		Thread thread3 = new AlphabetThread();
+		thread1.start();
+		thread2.start();
+		thread3.start();
+		
+	}
+}
+class DigitThread extends Thread{
+	public void run() {
+		for(int cnt=0; cnt<10; cnt++) {
+			System.out.print(cnt);
+			try {
+				Thread.sleep(1000);
+			}catch(InterruptedException e){
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+}
+class AlphabetThread extends Thread{
+	public void run() {
+		for(char ch='A'; ch <='Z'; ch++) {
+			System.out.print(ch);
+		}
+	}
+}
