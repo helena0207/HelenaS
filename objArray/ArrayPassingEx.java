46b3831fe4d020f486636c0a391cc479769756f2
helena <dineout@daum.net>
2020-07-07 17:07:30 +0900

    Java grammar

---
+++ objArray/ArrayPassingEx.java
@@ -0,0 +1,26 @@
+package objArray;
+public class ArrayPassingEx{
+	static void printCharArray(char a[]) {
+		for(int i=0; i<a.length;i++) {
+			System.out.print(a[i]);
+		}
+		System.out.println();
+	}
+	
+	static void replaceSpace(char a[]) {
+		for(int i=0;i<a.length; i++) {
+			if(a[i]==' ') {
+				a[i]=',';
+			}
+		}
+	}
+	
+	public static void main(String args[]) {
+		char c[]= {'T','h','i','s',' ','p','e','n','c','i','l'};
+		printCharArray(c);
+		replaceSpace(c);
+		printCharArray(c);
+	}
+}
+
+//
---
+++ objArray/ThisArray.java
@@ -0,0 +1,34 @@
+package objArray;
+
+import java.util.Scanner;
+
+class Book{
+	String title, author;
+
+	public Book(String title, String author) {
+		super();
+		this.title = title;
+		this.author = author;
+	}
+}
+
+public class ThisArray{
+	public static void main(String[] args) {
+		Book[] book=new Book[2];
+		
+		Scanner scanner=new Scanner(System.in);
+		for(int i=0; i<book.length; i++) {
+			System.out.print("제목>>");
+			String title = scanner.nextLine();
+			System.out.print("저자>>");
+			String author = scanner.nextLine();
+			book[i] =new Book(title, author);
+		}
+		
+		for(int i=0; i<book.length; i++) {
+			System.out.print("("+book[i].title +"," + book[i].author+")");
+		}
+		scanner.close();
+		
+	}
+}
